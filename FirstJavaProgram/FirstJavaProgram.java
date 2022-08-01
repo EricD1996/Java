f1c5abe672774866eab96e6d121b83d1e7966a74
EricD1996 <EricD8816051@gmail.com>
2022-08-01 10:36:30 -0700

    first commit

---
+++ FirstJavaProgram/FirstJavaProgram.java
@@ -0,0 +1,13 @@
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("My name is Coding Dojo");
+        System.out.println("I am 100 years old");
+        System.out.println("My hometown is Burbank, CA");
+    }
+}
+
+
+// My name is Coding Dojo
+// I am 100 years old
+// My hometown is Burbank, CA
+
